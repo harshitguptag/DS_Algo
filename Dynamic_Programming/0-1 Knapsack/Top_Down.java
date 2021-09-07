4ebac6e6bf16cec7f9f9a859c8d5acdbde09f214
Devesh Gupta <devesh.gupta99@gmail.com>
2021-09-07 19:46:02 +0530

    Updated Top-Down approach

---
+++ Dynamic_Programming/0-1 Knapsack/Top_Down.java
@@ -0,0 +1,11 @@
+public class Top_Down
+{
+
+       public static void main(String args[])
+       {
+            int val[] = new int[] {15, 14, 10, 45, 30};
+            int wt[] = new int[] {2, 5, 1, 3, 4};
+            int n=val.length;
+            int W=7;
+       }
+}
