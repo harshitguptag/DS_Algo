7313df4bb098216473d6545a36b9e48dec406eab
Devesh Gupta <devesh.gupta99@gmail.com>
2021-09-17 21:15:59 +0530

    Recurssion

---
+++ Recurssion/Power_Log.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Power_Log
+{
+  public static void main(String[] args) throws Exception 
+  {
+    Scanner scn = new Scanner(System.in);
+    int x = scn.nextInt();
+    int n = scn.nextInt();
+    int p = power(x, n);
+    System.out.println(p);
+  }
+
+  public static int power(int x, int n) 
+  {
+    if(n == 0)
+    {
+      return 1;
+    }
+    int xpnb2 = power(x, n / 2);
+    int xpn = xpnb2 * xpnb2;
+
+    if(n % 2 == 1)
+    {
+      xpn = xpn * x;
+    }
+    return xpn;
+  }
+}
+
+
+                        
